264272416a6732fe44b1fa8254b1760d168a4023
mcBorgar <borgarfb@gmail.com>
2024-10-10 09:10:54 +0200

    Update applikasjon.py

--- applikasjon.py
+++ applikasjon.py
@@ -1,12 +1,16 @@
-#!/bin/bash
-
 import tkinter as tk
 
 root = tk.Tk()
-root.title("min første app")
+root.title("PythonApp")
+
+root.geometry("400x300")
 
 label = tk.Label(root, text="Hallo")
 label.pack()
+
+button = tk.Button(root, text="Button", command=lambda:
+label.config(text="Button Pressed"))
+button.pack()
 
 root.mainloop()
 
